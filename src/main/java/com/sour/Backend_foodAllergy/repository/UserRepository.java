7653a691073fb8729723a65dd786828fbdd2d86e
sami3l <elhadraoui.sami@emsi-edu.ma>
2025-05-13 16:26:35 +0100

    1

--- src/main/java/com/sour/Backend_foodAllergy/repository/UserRepository.java
+++ src/main/java/com/sour/Backend_foodAllergy/repository/UserRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 
-public interface UserRepository extends MongoRepository<User, Long> {
-    Optional<User> findById(String  objectId);
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findById(String  Id);
 
     Optional<User> findByUsername(String username);
 }
